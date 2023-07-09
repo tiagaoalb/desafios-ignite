3a55bcf2e0d26947f7c13e242d0fe0664eea90c9
Tiago <tiagaoalb@gmail.com>
2023-07-09 12:46:40 -0300

    chore: update is completed logic

--- todo-list/src/components/TaskCard.tsx
+++ todo-list/src/components/TaskCard.tsx
@@ -1,21 +1,26 @@
 import styles from './TaskCard.module.css'
-// import Checked from '../assets/checked.svg'
+import Checked from '../assets/checked.svg'
 import Unchecked from '../assets/unchecked.svg'
 import TrashCan from '../assets/trash.svg'
 
 interface TaskCardProps {
   description: string
   isComplete: boolean
+  toggleCompletion: () => void
 }
 
-export function TaskCard({ description, isComplete }: TaskCardProps) {
+export function TaskCard({
+  description,
+  isComplete,
+  toggleCompletion,
+}: TaskCardProps) {
   return (
     <div className={styles.card}>
       <div className={styles.content}>
-        <button>
-          <img src={Unchecked} />
+        <button onClick={toggleCompletion}>
+          {isComplete ? <img src={Checked} /> : <img src={Unchecked} />}
         </button>
-        <span>{description}</span>
+        <span className={isComplete ? styles.completed: ''}>{description}</span>
         <button>
           <img src={TrashCan} />
         </button>
