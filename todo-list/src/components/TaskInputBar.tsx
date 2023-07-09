0698779a0370e29541a6098d6c14672413cafc74
Tiago <tiagaoalb@gmail.com>
2023-07-09 12:46:07 -0300

    feat: add disabled button functions

--- todo-list/src/components/TaskInputBar.tsx
+++ todo-list/src/components/TaskInputBar.tsx
@@ -1,14 +1,32 @@
 import styles from './TaskInputBar.module.css'
 import plus from '../assets/plus.svg'
+import { useState } from 'react'
 
 export function TaskInputBar() {
+  const [inputValue, setInputValue] = useState('')
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value)
+  }
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+  }
+
   return (
     <div className={styles.taskinput}>
-      <input type="text" placeholder="Adicione uma nova tarefa" />
-      <button type="submit">
-        Criar
-        <img src={plus} alt="" />
-      </button>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Adicione uma nova tarefa"
+          value={inputValue}
+          onChange={handleInputChange}
+        />
+        <button type="submit" disabled={!inputValue}>
+          Criar
+          <img src={plus} alt="" />
+        </button>
+      </form>
     </div>
   )
 }
